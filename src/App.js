f13cd19db744ffd857e51125ddd8ef756553e897
NeNeZoRr <zorrillalin@gmail.com>
2023-12-11 09:28:55 -0500

    polish and details

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import { useState, useEffect } from 'react'
-import Gallery from './Gallery'
-import ButtonBar from './ButtonBar'
+import React, { useState, useEffect } from 'react';
+import Gallery from './Gallery';
+import ButtonBar from './ButtonBar';
 
 function App() {
-  let [data, setData] = useState({})
-  let [artId, setArtId] = useState(12770)
+  let [data, setData] = useState({});
+  let [artId, setArtId] = useState(12770);
 
   useEffect(() => {
-    document.title = 'Welcome to ArtWorld'
+    document.title = 'Welcome to ArtWorld';
     fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
       .then(response => response.json())
-      .then(resdata => setData(resdata))
-  }, [artId])
+      .then(resdata => setData(resdata));
+  }, [artId]);
 
   const handleIterate = (e) => {
-    setArtId(artId + Number(e.target.value))
-  }
+    setArtId(artId + Number(e.target.value));
+  };
 
   const displayImage = () => {
     if (!data.primaryImage) {
-      return (
-        <h2>No Image!</h2>
-      )
+      return <h2>No Image!</h2>;
     }
-    return (
-      <Gallery objectImg={data.primaryImage} title={data.title} />
-    )
-  }
+    return <Gallery objectImg={data.primaryImage} title={data.title} />;
+  };
 
   return (
-    <div className="App">
-      <h1>{data.title}</h1>
-      <div style={{ 'width': '100%' }}>
+    <div className="App" style={{ textAlign: 'center' }}>
+      <div style={{ marginTop: '50px' }}>
+        <h1>{data.title}</h1>
+      </div>
+      <div style={{ width: '100%' }}>
         {displayImage()}
       </div>
       <ButtonBar handleIterate={handleIterate} />
--- src/ButtonBar.js
+++ src/ButtonBar.js
@@ -1,6 +1,6 @@
 const ButtonBar = (props) => {
     return (
-        <div>
+        <div style={{ display: 'flex', justifyContent: 'center', position: 'fixed', bottom: 0, width: '100%', padding: '10px', background: 'white' }}>
             <button value={-5} onClick={props.handleIterate}>Back, 5</button>
             <button value={-1} onClick={props.handleIterate}>Back</button>
             <button value={1} onClick={props.handleIterate}>Next</button>
